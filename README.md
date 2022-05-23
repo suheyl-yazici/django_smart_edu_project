8ffa26c4a00deb5e3e1977898ff712580f5bf4c7
suheyl-yazici <h.suheylyazici@gmail.com>
2022-05-23 19:30:04 +0300

    django prejesine başlandı

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django_smart_edu_project
+
+5.videodayım
---
+++ smartedu_con/pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path(route,view,opt(kısa yol ismi)),
+]
