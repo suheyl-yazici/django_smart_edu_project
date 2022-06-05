f65137e6c1f0db3fbf97284e72b6a387b6b0ece5
suheyl-yazici <h.suheylyazici@gmail.com>
2022-06-05 13:31:25 +0300

    Kursa kayıt yapıldı

--- smartedu_con/accounts/views.py
+++ smartedu_con/accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from courses.models import Course
+from django.contrib.auth.models import User
 
 
 def user_login(request):
@@ -44,6 +46,23 @@ def user_logout(request):
     logout(request)
     return redirect('index')
 
-
+@login_required(login_url='login')
 def user_dashboard(request):
-    pass
+    current_user = request.user
+
+    courses = current_user.courses_joined.all()
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'dashboard.html', context)
+
+
+def enroll_the_course(request):
+    course_id = request.POST["course_id"]
+    user_id = request.POST["user_id"]
+    course = Course.objects.get(id=course_id)
+    user = User.objects.get(id=user_id)
+    course.students.add(user)
+    return redirect('dashboard')
+
